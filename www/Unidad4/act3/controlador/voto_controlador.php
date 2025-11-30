e220063b8e78f3aa997be997262b1b185dab1391
NicoJG27 <njimgut1905@g.educaand.es>
2025-11-30 13:50:16 +0100

    todo perfec

--- www/Unidad4/act3/controlador/voto_controlador.php
+++ www/Unidad4/act3/controlador/voto_controlador.php
@@ -11,29 +11,23 @@ class VoteController
 
     public function ejecutar()
     {
-        // 1. GESTIÓN DE PETICIONES (POST)
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             
-            // CORRECCIÓN: Ahora buscamos 'voto' (el radio button de tu vista)
             if (isset($_POST['voto'])) {
                 $this->model->votar($_POST['voto']);
             }
 
-            // CORRECCIÓN: Ahora buscamos 'eliminar' (el botón rojo de tu vista)
             if (isset($_POST['eliminar'])) {
                 $this->model->eliminar($_POST['eliminar']);
             }
 
-            // CORRECCIÓN: Ahora buscamos 'texto' (el input de agregar de tu vista)
             if (isset($_POST['texto']) && !empty($_POST['texto'])) {
                 $this->model->agregarOpcion($_POST['texto']);
             }
         }
 
-        // 2. Obtener datos
         $listaOpciones = $this->model->obtenerOpciones();
 
-        // 3. Calcular total
         $totalVotos = 0;
         if ($listaOpciones) {
             foreach ($listaOpciones as $opcion) {
@@ -41,7 +35,6 @@ class VoteController
             }
         }
 
-        // 4. Devolver datos
         return [
             'opciones' => $listaOpciones,
             'total'    => $totalVotos
--- www/Unidad4/act3/public/index.php
+++ www/Unidad4/act3/public/index.php
@@ -5,15 +5,11 @@ require_once __DIR__ .'/../modelo/optionmodel.php';
 require_once __DIR__ .'/../controlador/voto_controlador.php'; 
 
 try {
-    // 2. Conexión a la Base de Datos
     $pdo = new PDO(DB_DSN, DB_USER, DB_PASS);
     
-    // 3. Preparamos las piezas (Modelo y Controlador)
     $modelo = new optionModel($pdo);
     $controlador = new VoteController($modelo);
 
-    // 4. Ejecutamos el controlador y guardamos lo que devuelve en $data
-    // Esto es clave: $data tendrá dentro ['opciones'] y ['total']
     $data = $controlador->ejecutar();
 
 
